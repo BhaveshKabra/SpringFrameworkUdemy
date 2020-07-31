534f8586f43c831d9066319bdc9ebeba783dcc8a
Bhavesh Kabra <kabrabhavesh12@gmail.com>
2020-07-31 18:57:36 +0530

    Test

---
+++ src/main/java/org/bhavesh/SpringFrameworkUdemyApplication.java
@@ -0,0 +1,13 @@
+package org.bhavesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkUdemyApplication.class, args);
+	}
+
+}
